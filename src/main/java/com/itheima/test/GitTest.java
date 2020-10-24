746fdb8c970192df445cf526016412471e3da8b6
yuanjian <yuanjianstu@163.com>
2020-10-24 13:42:45 +0800

    新增：第一次提交

---
+++ src/main/java/com/itheima/test/GitTest.java
@@ -0,0 +1,12 @@
+package com.itheima.test;
+
+/**
+ * @author ：yuanjian
+ * @date ：Created in 2020/10/24 13:38
+ * @description：
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("idea远程连接GitHub!!!");
+    }
+}
